bf1c34ee00972714e87eafcda06b2462157e548a
gdanezis <g.danezis@ucl.ac.uk>
2017-05-24 11:55:49 +0100

    Added functions to encode, decode, and check private claims

--- claimchain/__init__.py
+++ claimchain/__init__.py
@@ -34,6 +34,34 @@ def VRF_verify(G, pub, message, value, proof):
 	s2 = Bn.from_binary(sha256(encode([ g, h, pub, v, R, Hr ])).digest())
 	return s2 == s
 
+from petlib.cipher import Cipher
 
-def void():
-	pass
+def encode_claim(G, pub, k, nonce, claim_key, claim_body):
+	aes = Cipher("aes-128-gcm")
+	nkey = encode([nonce, claim_key])
+	vrfkey, proof = VRF_compute(G, k, pub, nkey)
+	lookup_key = sha256("LKey||" + vrfkey).digest()[:8]
+	encryption_key = sha256("EKey||" + vrfkey).digest()[:16]
+
+	claim = encode([proof, claim_body])
+	encbody, tag = aes.quick_gcm_enc(encryption_key, b"\x00"*16, claim)
+
+	return (vrfkey, lookup_key, encbody, tag)
+
+def decode_claim(G, pub, nonce, claim_key, vrfkey, encrypted_body, tag):
+	aes = Cipher("aes-128-gcm")
+	nkey = encode([nonce, claim_key])
+	
+	# vrfkey, proof = VRF_compute(G, k, pub, nkey)
+	lookup_key = sha256("LKey||" + vrfkey).digest()[:8]
+	encryption_key = sha256("EKey||" + vrfkey).digest()[:16]
+
+	#claim = encode([vrfkey, proof, claim_key, claim_body])
+	decbody = aes.quick_gcm_dec(encryption_key, b"\x00"*16, encrypted_body, tag)
+	[proof, claim_body] = decode(decbody)
+
+	nkey = encode([nonce, claim_key])
+	if not VRF_verify(G, pub, nkey, vrfkey, proof):
+		raise Exception("Wrong VRF Value")
+
+	return claim_body
--- claimchain/test/test_core.py
+++ claimchain/test/test_core.py
@@ -1,5 +1,5 @@
 from petlib.ec import EcGroup
-from .. import VRF_compute, VRF_verify
+from .. import VRF_compute, VRF_verify, encode_claim, decode_claim
 
 def test_access():
 	assert True
@@ -10,3 +10,16 @@ def test_VRF():
 	pub = k * G.generator()
 	value, proof = VRF_compute(G, k, pub, "test@test.com")
 	assert VRF_verify(G, pub, "test@test.com", value, proof)
+
+def test_encode_claim():
+	G = EcGroup()
+	k = G.order().random()
+	pub = k * G.generator()
+	nonce = "xxx"
+	claim_key = "george@gmail.com"
+	claim_body = "XXXXX"
+
+	enc = encode_claim(G, pub, k, nonce, claim_key, claim_body)
+	vrfkey, lookupkey, encrypted_body, tag  = enc
+
+	decode_claim(G, pub, nonce, claim_key, vrfkey, encrypted_body, tag)
