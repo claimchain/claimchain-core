a94577668a8be5534d66979ec5955905f9827041
gdanezis <g.danezis@ucl.ac.uk>
2017-05-24 11:24:23 +0100

    Serilaize the VRF to make life easy

--- claimchain/__init__.py
+++ claimchain/__init__.py
@@ -2,7 +2,7 @@
 
 from petlib.ec import EcGroup, EcPt
 from petlib.bn import Bn
-from petlib.pack import encode
+from petlib.pack import encode, decode
 
 from hashlib import sha256
 
@@ -22,19 +22,18 @@ def VRF_compute(G, k, pub, message):
 	Hr = r * h
 	s = Bn.from_binary(sha256(encode([ g, h, pub, v, R, Hr ])).digest())
 	t = (r - s * k) % G.order()
-	return v, (s, t)
+	return v.export(), encode((s, t))
 
 def VRF_verify(G, pub, message, value, proof):
 	g = G.generator()
 	h = G.hash_to_point("1||" + message)
-	v = value
-	s, t = proof
-
+	v = EcPt.from_binary(value, G)
+	s, t = decode(proof)
 	R = t*g + s*pub
 	Hr = t*h + s*v
 	s2 = Bn.from_binary(sha256(encode([ g, h, pub, v, R, Hr ])).digest())
-
 	return s2 == s
+
 
 def void():
 	pass
