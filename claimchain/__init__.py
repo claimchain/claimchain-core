789e02b68bfed05a663979e97af67908e5442562
gdanezis <g.danezis@ucl.ac.uk>
2017-05-24 10:47:42 +0100

    Added fisrt files

---
+++ claimchain/__init__.py
@@ -0,0 +1,14 @@
+# The claimchain core package
+
+import petlib
+import pblib
+
+model_claim_block = {
+	"Version": "1.0",
+	"Metadata": None,
+	"TreeHead": None,
+	"ChainInfo": None
+}
+
+def void():
+	pass
