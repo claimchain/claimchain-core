d9ff0867fa25013a848aec45a7744bc27fcab65e
gdanezis <g.danezis@ucl.ac.uk>
2017-05-24 11:18:48 +0100

    Added VRF

--- claimchain/__init__.py
+++ claimchain/__init__.py
@@ -1,7 +1,10 @@
 # The claimchain core package
 
-import petlib
-import pblib
+from petlib.ec import EcGroup, EcPt
+from petlib.bn import Bn
+from petlib.pack import encode
+
+from hashlib import sha256
 
 model_claim_block = {
 	"Version": "1.0",
@@ -9,6 +12,29 @@ model_claim_block = {
 	"TreeHead": None,
 	"ChainInfo": None
 }
+
+def VRF_compute(G, k, pub, message):
+	g = G.generator()
+	h = G.hash_to_point("1||" + message)
+	v = k * h
+	r = G.order().random()
+	R = r * g
+	Hr = r * h
+	s = Bn.from_binary(sha256(encode([ g, h, pub, v, R, Hr ])).digest())
+	t = (r - s * k) % G.order()
+	return v, (s, t)
+
+def VRF_verify(G, pub, message, value, proof):
+	g = G.generator()
+	h = G.hash_to_point("1||" + message)
+	v = value
+	s, t = proof
+
+	R = t*g + s*pub
+	Hr = t*h + s*v
+	s2 = Bn.from_binary(sha256(encode([ g, h, pub, v, R, Hr ])).digest())
+
+	return s2 == s
 
 def void():
 	pass
--- claimchain/test/test_core.py
+++ claimchain/test/test_core.py
@@ -1,2 +1,12 @@
+from petlib.ec import EcGroup
+from .. import VRF_compute, VRF_verify
+
 def test_access():
 	assert True
+
+def test_VRF():
+	G = EcGroup()
+	k = G.order().random()
+	pub = k * G.generator()
+	value, proof = VRF_compute(G, k, pub, "test@test.com")
+	assert VRF_verify(G, pub, "test@test.com", value, proof)
