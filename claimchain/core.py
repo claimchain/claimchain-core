4685a5c14ef317993bb762984607616b1783abcf
Bogdan Kulynych <hello@bogdankulynych.me>
2017-05-30 17:42:54 +0200

    Encapsulate hash and key length parameters; Standardize hash prefixes

--- claimchain/core.py
+++ claimchain/core.py
@@ -14,12 +14,12 @@ def encode_claim(nonce, claim_label, claim_body):
     pp = PublicParams.get_default()
     nkey = encode([nonce, claim_label])
     vrf = do_vrf_compute(nkey)
-    lookup_key = sha256(b"LKey||" + vrf.value).digest()[:8]
-    encryption_key = sha256(b"EKey||" + vrf.value).digest()[:16]
+    lookup_key = sha256(b"clm_lookup|" + vrf.value).digest()[:pp.short_hash_size]
+    enc_key = sha256(b"clm_enc_key|" + vrf.value).digest()[:pp.enc_key_size]
 
     claim = encode([vrf.proof, claim_body])
     encbody, tag = pp.enc_cipher.quick_gcm_enc(
-            encryption_key, b"\x00"*16, claim)
+            enc_key, b"\x00"*pp.enc_key_size, claim)
 
     all_body = encode([encbody, tag])
     return (vrf.value, lookup_key, all_body)
@@ -27,14 +27,15 @@ def encode_claim(nonce, claim_label, claim_body):
 
 def decode_claim(owner_vrf_pk, nonce, claim_label, vrf_value, encrypted_claim):
     pp = PublicParams.get_default()
-    aes = pp.enc_cipher
+    cipher = pp.enc_cipher
     nkey = encode((nonce, claim_label))
     (encrypted_body, tag) = decode(encrypted_claim)
 
-    lookup_key = sha256(b"LKey||" + vrf_value).digest()[:8]
-    encryption_key = sha256(b"EKey||" + vrf_value).digest()[:16]
+    lookup_key = sha256(b"clm_lookup|" + vrf_value).digest()[:pp.short_hash_size]
+    enc_key = sha256(b"clm_enc_key|" + vrf_value).digest()[:pp.enc_key_size]
 
-    raw_body = aes.quick_gcm_dec(encryption_key, b"\x00"*16, encrypted_body, tag)
+    raw_body = cipher.quick_gcm_dec(
+            enc_key, b"\x00"*pp.enc_key_size, encrypted_body, tag)
     (proof, claim_body) = decode(raw_body)
 
     vrf = VrfContainer(value=vrf_value, proof=proof)
@@ -45,42 +46,44 @@ def decode_claim(owner_vrf_pk, nonce, claim_label, vrf_value, encrypted_claim):
 
 
 def get_capability_lookup_key(owner_dh_pk, nonce, claim_label):
+    pp = PublicParams.get_default()
     params = LocalParams.get_default()
     shared_secret = params.dh.sk * owner_dh_pk
-    encryption_key = sha256(shared_secret.export()).digest()[:16]
+    shared_secret_key = sha256(shared_secret.export()).digest()
 
-    lookup_key = sha256(b"lookup|%s|%s|%s" % (
-            encryption_key, nonce, claim_label)).digest()
+    lookup_key = sha256(b"cap_lookup|%s|%s|%s" % (
+        shared_secret_key, nonce, claim_label)).digest()[:pp.short_hash_size]
     return lookup_key
 
 
 def encode_capability(reader_dh_pk, nonce, claim_label, vrf_value):
     pp = PublicParams.get_default()
-    aes = pp.enc_cipher
+    cipher = pp.enc_cipher
     params = LocalParams.get_default()
 
     shared_secret = params.dh.sk * reader_dh_pk
-    shared_secret_key = sha256(shared_secret.export()).digest()[:16]
-    # TODO: Why this is not truncated?
-    lookup_key = sha256(b"lookup|%s|%s|%s" %
-            (shared_secret_key, nonce, claim_label)).digest()
-    encryption_key = sha256(b"key|%s|%s|%s" %
-            (shared_secret_key, nonce, claim_label)).digest()[:16]
+    shared_secret_key = sha256(shared_secret.export()).digest()
+    lookup_key = sha256(b"cap_lookup|%s|%s|%s" %
+            (shared_secret_key, nonce, claim_label)).digest()[:pp.short_hash_size]
+    enc_key = sha256(b"cap_enc_key|%s|%s|%s" %
+            (shared_secret_key, nonce, claim_label)).digest()[:pp.enc_key_size]
 
-    encbody, tag = aes.quick_gcm_enc(encryption_key, b"\x00"*16, vrf_value)
+    encbody, tag = cipher.quick_gcm_enc(
+            enc_key, b"\x00"*pp.enc_key_size, vrf_value)
     return lookup_key, encode([encbody, tag])
 
 
 def decode_capability(owner_dh_pk, nonce, claim_label, encrypted_capability):
     pp = PublicParams.get_default()
-    aes = pp.enc_cipher
+    cipher = pp.enc_cipher
     params = LocalParams.get_default()
     shared_secret = params.dh.sk * owner_dh_pk
-    shared_secret_key = sha256(shared_secret.export()).digest()[:16]
+    shared_secret_key = sha256(shared_secret.export()).digest()
 
-    secret_key = sha256(b"key|%s|%s|%s" %
-            (shared_secret_key, nonce, claim_label)).digest()[:16]
+    enc_key = sha256(b"cap_enc_key|%s|%s|%s" %
+            (shared_secret_key, nonce, claim_label)).digest()[:pp.enc_key_size]
 
     encbody, tag = decode(encrypted_capability)
-    body = aes.quick_gcm_dec(secret_key, b"\x00"*16, encbody, tag)
+    body = cipher.quick_gcm_dec(
+            enc_key, b"\x00"*pp.enc_key_size, encbody, tag)
     return body
