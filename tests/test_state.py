7cdecf08cce9eb225cefb546264718c967bec3cc
Bogdan Kulynych <hello@bogdankulynych.me>
2017-06-02 14:12:53 +0200

    Add failing State tests

--- tests/test_state.py
+++ tests/test_state.py
@@ -4,7 +4,7 @@ import hippiehug
 from petlib.pack import encode, decode
 
 from claimchain.state import State
-from claimchain.core import get_capability_lookup_key
+from claimchain.core import get_capability_lookup_key, encode_claim, _compute_claim_key
 from claimchain.crypto import PublicParams, LocalParams, do_vrf_compute
 from claimchain.utils import ascii2bytes
 
@@ -57,7 +57,6 @@ def commit_claims(state, claims):
     block_content = block.items[0]
     nonce = ascii2bytes(block_content['payload']['nonce'])
 
-    # Get associated Merkle tree
     mtr_hash = ascii2bytes(block.items[0]['payload']['mtr_hash'])
     tree = hippiehug.Tree(store, root_hash=mtr_hash)
     return nonce, chain, tree
@@ -65,18 +64,19 @@ def commit_claims(state, claims):
 
 def test_tree_contains_claim_lookup_key(state):
     nonce, chain, tree = commit_claims(state, [("marios", "test")])
-
-    salted_label = encode([nonce, "marios"])
-    vrf = do_vrf_compute(salted_label)
-    pp = PublicParams.get_default()
-
-    # TODO: Refactor into a separate function
-    lookup_key = pp.hash_func(b"clm_lookup|" + vrf.value).digest()[:pp.short_hash_size]
-    enc_key = pp.hash_func(b"clm_enc_key|" + vrf.value).digest()[:pp.enc_key_size]
-
-    evidence = tree.evidence(key=lookup_key)
-    leaf = evidence[1][-1]
+    vrf_value, lookup_key, enc_claim = encode_claim(nonce, "marios", "test")
+    value, evidence = tree.evidence(key=lookup_key)
+    leaf = evidence[-1]
     assert leaf.key == lookup_key
+
+    # TODO: Uncomment once hippiehug doesn't put item hashes on the tree
+    # enc_key = _compute_claim_key(vrf_value, mode='enc')
+    # enc_body, tag = decode(leaf.item)
+    # pp = PublicParams.get_default()
+    # raw_body = pp.cipher.quick_gcm_dec(
+    #         enc_key, b"\x00"*pp.enc_key_size, encrypted_body, tag)
+    # (proof, claim_content) = decode(raw_body)
+    # assert claim_content == "test"
 
 
 def test_tree_contains_cap_lookup_key(state):
@@ -90,7 +90,8 @@ def test_tree_contains_cap_lookup_key(state):
     with reader_params.as_default():
         lookup_key = get_capability_lookup_key(owner_pk, nonce, "marios")
 
-    evidence = tree.evidence(key=lookup_key)
-    leaf = evidence[1][-1]
+    value, evidence = tree.evidence(key=lookup_key)
+    leaf = evidence[-1]
     assert leaf.key == lookup_key
 
+    # TODO: Test value similarly to line 72
