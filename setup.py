12acbc70e84255f87b89b15375d46e825a70e8a1
holger krekel <holger@merlinux.eu>
2018-03-19 15:55:16 +0100

    fix packaging

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='claimchain',
     version='0.1.2',
-    packages=['claimchain',],
+    packages=["claimchain", "claimchain.crypto", "claimchain.utils"],
     license='MIT',
     description='Core and experimental implementation of ClaimChain, decentralized PKI',
     long_description=long_description,
