206e40458b0171d2eace339b88112f53f54ffc98
Bogdan Kulynych <hello@bogdankulynych.me>
2018-03-26 19:15:58 +0200

    Refresh setup.py a bit

--- setup.py
+++ setup.py
@@ -3,37 +3,48 @@ from codecs import open
 from setuptools import setup, find_packages
 
 
+INSTALL_REQUIRES = [
+    'six',
+    'petlib',
+    'pyyaml',
+    'attrs',
+    'base58',
+    'statistics',
+    'defaultcontext>=1.1.0',
+]
+
+
 here = path.abspath(path.dirname(__file__))
-
-
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 
 setup(
     name='claimchain',
-    version='0.1dev',
-    packages=['claimchain',],
+    version='1.0',
+    packages=['claimchain'],
     license='MIT',
-    description='Core and experimental implementation of ClaimChain, decentralized PKI',
+    description=('Core and experimental implementation of ClaimChain, '
+                 'decentralized privacy-preserving key distribution.',
     long_description=long_description,
     author='George Danezis',
     author_email='g.danezis@ucl.ac.uk',
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Security :: Cryptography'
+        "Development Status :: 3 - Alpha",
+	"Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+	"Natural Language :: English",
+	"Operating System :: OS Independent",
+	"Programming Language :: Python",
+	"Programming Language :: Python :: 2",
+	"Programming Language :: Python :: 2.7",
+	"Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 3.4",
+	"Programming Language :: Python :: 3.5",
+	"Programming Language :: Python :: 3.6",
+	"Programming Language :: Python :: Implementation :: CPython",
+	"Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Security :: Cryptography",
     ],
-    install_requires=[
-        'six',
-        'petlib',
-        'pyyaml',
-        'attrs',
-        'base58',
-        'statistics',
-        'defaultcontext',
-        'hippiehug >= 0.1.1',
-    ],
-
+    install_requires=INSTALL_REQUIRES,
 )
-
