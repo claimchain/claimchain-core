af3aa59c90c2d6711fc3893ee63a61a238a333f3
Bogdan Kulynych <hello@bogdankulynych.me>
2018-07-17 12:11:26 +0200

    Fix the ZK

--- claimchain/zk.py
+++ claimchain/zk.py
@@ -1,6 +1,7 @@
 """
 Claimchain zero-knowledge proofs.
 """
+
 import os
 import attr
 
@@ -26,9 +27,14 @@ class ClaimProofContainer(object):
 def compute_claim_proof(nonce, claim_label, claim_body):
     """Compute a claim proof.
 
-    Produces a VRF proof of the lookup key and the ZK proof of the claim content
-    commitment correctness.
+    Produces a VRF proof of the lookup key and the signature ZK proof
+    of the claim content commitment correctness.
+
+    :param bytes nonce: Nonce
+    :param bytes claim_label: Claim label
+    :param bytes claim_body: Claim body
     """
+
     pp = PublicParams.get_default()
     local_params = LocalParams.get_default()
     nonce = ensure_binary(nonce)
@@ -37,14 +43,14 @@ def compute_claim_proof(nonce, claim_label, claim_body):
 
     G = pp.ec_group
     g = G.generator()
-    a = G.hash_to_point('a')
-    b = G.hash_to_point('b')
+    a = G.hash_to_point(b'a')
+    b = G.hash_to_point(b'b')
+    z = G.hash_to_point(nonce + claim_label)
 
-    k = local_params.vrf.sk
+    sk = local_params.vrf.sk
     pk = local_params.vrf.pk
-    v = G.hash_to_point(nonce + claim_label)
 
-    h = k * v
+    h = sk * z
 
     alpha = G.order().random()
     bind = Bn.from_binary(sha256(claim_body).digest()) * a
@@ -55,18 +61,18 @@ def compute_claim_proof(nonce, claim_label, claim_body):
     proof_key = os.urandom(pp.nonce_size)
 
     R_pk = r_sk * g
-    R_h = r_sk * h
+    R_h = r_sk * z
     R_com = r_alpha * b
 
     packed_challenge = [
-        g, v, a, b,
+        g, z, a, b,
         pk, h, com, bind,
         R_pk, R_h, R_com,
         proof_key
     ]
 
     c = Bn.from_binary(sha512(encode(packed_challenge)).digest())
-    s_sk = (r_sk - c * k) % G.order()
+    s_sk = (r_sk - c * sk) % G.order()
     s_alpha = (r_alpha - c * alpha) % G.order()
     return ClaimProofContainer(
             vrf_value=h,
@@ -77,27 +83,35 @@ def compute_claim_proof(nonce, claim_label, claim_body):
 
 def verify_claim_proof(owner_vrf_pk, nonce, claim_proof, claim_label, claim_body):
     """Verify the claim proof.
+
+    :param owner_vrf_pk: Owner's VRF pk
+    :param bytes nonce: Random nonce
+    :param ClaimProofContainer claim_proof: Claim proof
+    :param bytes claim_label: Claim label
+    :param bytes claim_body: Claim body
     """
+
     pp = PublicParams.get_default()
     local_params = LocalParams.get_default()
+    nonce = ensure_binary(nonce)
     claim_label = ensure_binary(claim_label)
     claim_body = ensure_binary(claim_body)
 
     G = pp.ec_group
     g = G.generator()
-    a = G.hash_to_point('a')
-    b = G.hash_to_point('b')
+    a = G.hash_to_point(b'a')
+    b = G.hash_to_point(b'b')
+    z = G.hash_to_point(nonce + claim_label)
 
     c, s_sk, s_alpha = claim_proof.proof
-    v = G.hash_to_point(nonce + claim_label)
     R_pk = s_sk * g + c * owner_vrf_pk
-    R_h = s_sk * v + c * claim_proof.vrf_value
+    R_h = s_sk * z + c * claim_proof.vrf_value
 
     bind = Bn.from_binary(sha256(claim_body).digest()) * a
     R_com = s_alpha * b + c * (claim_proof.commitment - bind)
 
     packed_challenge = [
-        g, v, a,
+        g, z, a, b,
         owner_vrf_pk, claim_proof.vrf_value, claim_proof.commitment, bind,
         R_pk, R_h, R_com,
         claim_proof.proof_key
--- tests/test_zk.py
+++ tests/test_zk.py
@@ -11,29 +11,45 @@ def local_params():
 
 
 def test_proof_correct(local_params):
-    claim_proof = compute_claim_proof(b'42', b"test@test.com", b'hi there')
-    assert verify_claim_proof(local_params.vrf.pk, b'42', claim_proof, b"test@test.com", b'hi there')
+    claim_proof = compute_claim_proof('42', 'label', 'content')
+    assert verify_claim_proof(
+            local_params.vrf.pk, '42', claim_proof, 'label', 'content')
 
 
-# def test_vrf_incorrect_message(local_params):
-#     vrf = compute_vrf(b"test@test.com")
-#     assert not verify_vrf(local_params.vrf.pk, vrf, b"other@test.com")
+def test_proof_incorrect_label(local_params):
+    claim_proof = compute_claim_proof('42', 'label', 'content')
+    assert not verify_claim_proof(
+            local_params.vrf.pk, '1', claim_proof, 'label', 'content')
 
 
-# def test_vrf_incorrect_pubkey(local_params):
-#     vrf = compute_vrf(b"test@test.com")
-
-#     # Generate a distinct public key
-#     other_params = LocalParams.generate()
-#     while other_params.vrf.pk == local_params.vrf.pk:
-#         other_params = LocalParams.generate()
-
-#     assert not verify_vrf(other_params.vrf.pk, vrf,
-#             b"test@test.com")
+def test_proof_incorrect_label(local_params):
+    claim_proof = compute_claim_proof('42', 'label', 'content')
+    assert not verify_claim_proof(
+            local_params.vrf.pk, '42', claim_proof, 'incorrect label', 'content')
 
 
-# def test_vrf_deterministic_value(local_params):
-#     vrf1 = compute_vrf(b"test@test.com")
-#     vrf2 = compute_vrf(b"test@test.com")
-#     assert vrf1.value == vrf2.value
+def test_proof_incorrect_message(local_params):
+    claim_proof = compute_claim_proof('42', 'label', 'content')
+    assert not verify_claim_proof(
+            local_params.vrf.pk, '42', claim_proof, 'label', 'something else')
+
+
+def test_proof_incorrect_pubkey(local_params):
+    claim_proof = compute_claim_proof('42', 'label', 'content')
+    assert not verify_claim_proof(
+            local_params.vrf.pk, '42', claim_proof, 'label', 'something else')
+
+    # Generate a distinct public key
+    other_params = LocalParams.generate()
+    while other_params.vrf.pk == local_params.vrf.pk:
+        other_params = LocalParams.generate()
+
+    assert not verify_claim_proof(
+            other_params.vrf.pk, '42', claim_proof, 'label', 'content')
+
+
+def test_vrf_deterministic_value(local_params):
+    claim_proof1 = compute_claim_proof('42', 'label', 'content')
+    claim_proof2 = compute_claim_proof('42', 'label', 'content')
+    assert claim_proof1.vrf_value == claim_proof2.vrf_value
 
